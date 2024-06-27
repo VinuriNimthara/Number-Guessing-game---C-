649f2132b7199530751a7ea19e6a24080b04ca6e
Vinuri <Vinuri@MSI>
2024-06-27 12:00:14 +0530

    Add project files.

---
+++ Number Guessing game/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Guessing_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrectGuess = false;
+            Random random = new Random();
+
+            int randomNum = random.Next(1, 11);
+
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine ("A number between 1 and 10 will be generated");
+            Console.WriteLine("If you guess the correct number,You win!");
+                     
+            while (!isCorrectGuess)
+            {
+                Console.WriteLine("Please enter your guess.");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > randomNum)
+                {
+                    Console.WriteLine("Your guess is too high.");
+                }
+                else if (guess < randomNum)
+                {
+                    Console.WriteLine("Your guess is too low.");
+                }
+                else 
+                {
+                    Console.WriteLine("Correct!");
+                    isCorrectGuess = true;
+                }
+
+            }
+
+            Console.WriteLine("Congratulation,You have won the game!");
+
+            Console.ReadKey();
+        }
+
+    }
+        
+}
